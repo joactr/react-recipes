2f57e9fdb22dd6ea6a85cc0d73425553624e0bad
joactr <j.m.molol@hotmail.com>
2022-07-31 13:31:15 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Axios from "axios";
 
 function App() {
+  const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&includeNutrition=true`;
+
+  async function getRecipes(){
+    var result = await Axios.get(url);
+    console.log(result.data);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          onClick={getRecipes}
         >
           Learn React
         </a>
